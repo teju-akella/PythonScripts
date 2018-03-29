ad7da11a082e8aa51c7832bbef0174961d16bfb7
Teju_Akella <aakella@amphorainc.com>
2018-03-29 12:31:18 +0530

    completed on 29mar18

--- EvenAddBelow40lac.py
+++ EvenAddBelow40lac.py
@@ -1,29 +1,31 @@
 class EvenNumAdd:
-    def method1(self, number):
-        j=2
-        i=1
+    def method1(self):
+        j=1
+        i=0
         k=0
+        result=0
+        lo=0
 
-        for i in range(i,number):
-            temp=i+j
-            i=j
+        #for i in range(i,number):
+        while j<=4000000:
+            temp=k+j
+            k=j
             j=temp
             if j<=4000000:
-                if i%2==0:
-                    k=i+k
-                else:
-                    pass
+                if j%2==0:
+                    print(temp)
+                    result=result+j
 
             else:
                 break
 
 
-        print("value: " + str(k))
+        print("value: " + str(result))
 
 def main():
    # num=int(input("enter range"))
     en=EvenNumAdd()
-    en.method1(4000000)
+    en.method1()
 
 if __name__=='__main__':
     main()
